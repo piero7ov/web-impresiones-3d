915b857a7906adc340fc6fe99b518ec903356b74
Piero Olivares <piero7ov@gmail.com>
2026-02-19 20:28:13 +0100

    Create 001-web basica con PHP.php

---
+++ Iteracion/001-web basica con PHP.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Pierodev</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
