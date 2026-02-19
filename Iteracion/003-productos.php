f83446e08deeaffc501b22d5150ab82d8d273036
Piero Olivares <piero7ov@gmail.com>
2026-02-19 20:28:18 +0100

    Create 003-productos.php

---
+++ Iteracion/003-productos.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <title>Pierodev</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+    <header>
+    <h1>Pierodev | Tienda de Impresiones 3D</h1>
+    <nav>
+        <ul>
+            <li><a href="index.php">Inicio</a></li>
+            <li><a href="nosotros.php">Nosotros</a></li>
+            <li><a href="contacto.php">Contacto</a></li>
+        </ul>
+    </nav>
+    </header>
+    <main>
+        <article>
+        </article>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
